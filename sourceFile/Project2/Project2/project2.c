35d211551a14ba9459786f811fdab75bd76611dc
Hameli <hameli@naver.com>
2018-05-09 21:56:14 +0900

    Revert "Reupdate"
    This reverts commit 9606e294b702d3a6c618a0aea7894147e6c1b4f6.

--- sourceFile/Project2/Project2/project2.c
+++ sourceFile/Project2/Project2/project2.c
@@ -1,31 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"tag_ID_position.h"
 #pragma warning(disable:4996)
 
-//RFID 태그 정보를 저장하기 위한 구조체
 typedef struct taginfo {
 	char id[27];
 	float rssi;
 	double identifiedTime;
-	struct taginfo *next;
-	//struct taginfo *previous, *next;
+	struct taginfo *previous, *next;
 }TAGINFO;
 
-//이것도 괜춘할꺼 같은데? 아래꺼 쓰면 노쓸모
+//이것도 괜춘할꺼 같은데?
 //typedef struct List {
 //	TAGINFO tg;
 //	struct List *previous, *next;
 //}LIST;
 
-//RSSI 값 순으로 정렬시키기 위한 구조체
-typedef struct sortList {
-	char id[27];
-	int pos[2];
-	float rssi;
-	struct sortList *previous, *next;
-}SORTLIST;
 
 void initList();			//연결리스트 초기화
 void inserstList();			//연결리스트와 구조체 입력받아서 저장
@@ -33,7 +23,6 @@ void readFile();			//파일을 읽어오는 함수 (tagList, referList, sortList) 함수 내
 void referTagAnalysis();	//레퍼런스 태그 분석 기능
 void targetTagAnalysis();	//타겟 태그 분석 기능	taginfo 구조체 활용?
 void defineTagetPos();		//타겟 위치 추론
-void printInfo();			//태그내용 출력
 
 int main() {
 	char input;
