66f8c7288c48e88bd56d8e6d57913f8bce867414
Hameli <hameli@naver.com>
2018-06-06 20:17:26 +0900

    기본구조

---
+++ sourceFile/Project_Final/Project_Final/function.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+
+
+
+int fileExist(char *filename);
+void readTitle();
+void printMenu(char *filename);
+void readCustom();
+
+
+
+#endif
---
+++ sourceFile/Project_Final/Project_Final/main.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<signal.h>
+#include<time.h>
+#include<thread>
+#include"function.h"
+using namespace std;
+
+
+int main() {
+
+
+
+
+	return 0;
+}
