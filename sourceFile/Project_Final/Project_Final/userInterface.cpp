79f8922961a711cfb0936f462d676e4aca70c258
성소연 <sos980621@naver.com>
2018-06-09 21:29:22 +0900

    no message

---
+++ sourceFile/Project_Final/Project_Final/userInterface.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable 4996)
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	printf("◇D褻◆               ≒≒≒≒≒≒≒≒≒≒≒≒                    \n");
+	printf("                      ≒ 今 凶 今 勻 仆 太  ≒                ～    \n");
+	printf("                      ≒   中    仄     太  ≒             ９﹥﹥Ⅱ \n");
+	printf("                      ≒≒≒≒≒≒≒≒≒≒≒≒               ﹥﹥   \n");
+	printf("======================================================     ------------------\n");
+	printf("                                                                 ?  ?  ?     \n");
+	printf("                                                           ------------------\n");
+	printf("                                                           ﹤﹤﹤﹤﹤﹤﹤﹤﹤\n");
+	printf("                                                           ﹤﹤﹤﹥﹥﹥﹤﹤﹤\n");
+	printf("                                                           ﹤﹤﹥﹥﹥﹥﹥﹤﹤\n");
+	printf("                                                           ﹤﹤﹥﹥﹥﹥﹥﹤﹤\n");
+	printf("                                                           ﹤﹤﹥﹥﹥﹥﹥﹤﹤\n");
+	printf("                                                           ﹤﹤﹤﹥﹥﹥﹤﹤﹤\n");
+	printf("                                                           ﹤﹤﹤﹤﹥﹤﹤﹤﹤\n");
+	printf("                                                           ﹤﹤﹥﹥﹥﹥﹥﹤﹤\n");
+	printf("                                                           ﹤﹥﹥﹥﹥﹥﹥﹥﹤\n");
+	printf("                                                           ﹥﹥﹥﹥﹥﹥﹥﹥﹥\n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("                                                                             \n");
+	printf("======================================================                       \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	return 0;
+}
